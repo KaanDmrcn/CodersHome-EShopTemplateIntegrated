25ef68b7441aebdb11c03f7d65f1f701e26557c4
mr-aydemir <kaandemircan97@gmail.com>
2022-06-14 15:50:10 +0300

    Kategoriler Sayfası Eklendi

---
+++ eShopOnContainers/eShopOnContainers.Core/ViewModels/CategoriesViewModel.cs
@@ -0,0 +1,42 @@
+﻿using eShopOnContainers.Core.Models.PageCategory;
+using eShopOnContainers.Core.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace eShopOnContainers.Core.ViewModels
+{
+    public class CategoriesViewModel:ViewModelBase
+    {
+        private ObservableCollection<Category> _categories = new ObservableCollection<Category>()
+        {
+            new Category(){ Name = "Kupa", Id = 1},
+            new Category(){ Name = "Rozet", Id = 2},
+            new Category(){ Name = "Defter", Id = 3}
+        };
+        public ObservableCollection<Category> Categories
+        {
+            get => _categories;
+            set
+            {
+                _categories = value;
+                RaisePropertyChanged(() => Categories);
+            }
+        }
+        public override Task InitializeAsync(IDictionary<string, string> query)
+        {
+            return base.InitializeAsync(query);
+        }
+
+        public ICommand Search => new Command<Category>(async (item) =>
+        {
+            IsBusy = true;
+            await NavigationService.NavigateToAsync("Products", new Dictionary<string, string> { { "CategoryID", item.Id.ToString() } });
+            IsBusy = false;
+        });
+    }
+}
--- eShopOnContainers/eShopOnContainers.Core/ViewModels/ProductsViewModel.cs
+++ eShopOnContainers/eShopOnContainers.Core/ViewModels/ProductsViewModel.cs
@@ -21,7 +21,7 @@ namespace eShopOnContainers.Core.ViewModels
             new Product() { Id=2, Name = "Pardus Temalı Yazılımcı Kupası", Cost = 42.95, ImageURL= "https://i0.wp.com/www.codershome.net/wp-content/uploads/2022/03/pardus-1-min.jpg?fit=1600%2C1200;ssl=1", CategoryName= "Kupa",CategoryID=1},
             new Product() { Id=3, Name = "Flutter Temalı Yazılımcı İğneli Rozet", Cost = 7.50, ImageURL= "https://i0.wp.com/www.codershome.net/wp-content/uploads/2021/09/IMG_4120-min-scaled.jpg?fit=2560%2C2560;ssl=1", CategoryName= "Rozet",CategoryID=2},
             new Product() { Id=4, Name = "Don't Disturb Karton Kapaklı Yazılımcı Siyah Defter", Cost = 54.95, ImageURL= "https://i0.wp.com/www.codershome.net/wp-content/uploads/2021/09/dont-disturb-min.jpg?fit=1200%2C1200;ssl=1", CategoryName= "Defter",CategoryID=3},
-            new Product() { Id=5, Name = "Anti Coding Coding Club Yazılımcı Siyah Defter", Cost = 54.95, ImageURL= "https://i0.wp.com/www.codershome.net/wp-content/uploads/2021/09/anti-coding-min.jpg?fit=1200%2C1200;ssl=1", CategoryName= "Defter",CategoryID=4}
+            new Product() { Id=5, Name = "Anti Coding Coding Club Yazılımcı Siyah Defter", Cost = 54.95, ImageURL= "https://i0.wp.com/www.codershome.net/wp-content/uploads/2021/09/anti-coding-min.jpg?fit=1200%2C1200;ssl=1", CategoryName= "Defter",CategoryID=3}
             };
 
 
